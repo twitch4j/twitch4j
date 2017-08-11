edb3631640e701b011713669838f9b979c3bd4c6
Damian Staszewski <damian@stachuofficial.pl>
2017-08-11 18:05:35 +0200

    credential existence before connecting
    now `connect()` method will throw RuntimeException if oauth key doesn't exist.

--- src/main/java/me/philippheuer/twitch4j/TwitchClient.java
+++ src/main/java/me/philippheuer/twitch4j/TwitchClient.java
@@ -204,11 +204,14 @@ public class TwitchClient {
 	 * Connect needs to be called after initalizing the {@link CredentialManager}.
 	 */
 	public void connect() {
-		// Init IRC
-		setIrcClient(new IrcClient(this));
+		if (!getCredentialManager().getOAuthCredentials().containsKey("TWITCH-" + CREDENTIAL_IRC)) throw new RuntimeException("There is missing credentials to connecting chat. Please add it first.");
+		else {
+			// Init IRC
+			setIrcClient(new IrcClient(this));
 
-		// Init PubSub API
-		setPubSub(new TwitchPubSub(this));
+			// Init PubSub API
+			setPubSub(new TwitchPubSub(this));
+		}
 	}
 
 	/**
