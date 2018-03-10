b60b1aa37379a5e55030e9f5972b9c19c833bdb7
Flewp <alex@flewp.net>
2018-03-10 00:18:03 -0800

    Updating comment

--- src/main/java/me/philippheuer/twitch4j/endpoints/ChatEndpoint.java
+++ src/main/java/me/philippheuer/twitch4j/endpoints/ChatEndpoint.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 public class ChatEndpoint extends AbstractTwitchEndpoint {
 
 	/**
-	 * AbstractTwitchEndpoint
+	 * ChatEndpoint
 	 *
 	 * @param twitchClient todo
 	 */
