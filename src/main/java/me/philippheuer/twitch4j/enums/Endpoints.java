ea30f2346c8600f0640576e1c351895947879fa2
Damian Staszewski <damian@stachuofficial.pl>
2018-05-12 01:24:13 +0200

    docs(JAVADOC): adding colorized javadoc

--- src/main/java/me/philippheuer/twitch4j/enums/Endpoints.java
+++ src/main/java/me/philippheuer/twitch4j/enums/Endpoints.java
@@ -11,11 +11,16 @@ public enum Endpoints {
 	API("https://api.twitch.tv/kraken"), // default version api is v5
 	PUBSUB("wss://pubsub-edge.twitch.tv:443"),
 	IRC("wss://irc-ws.chat.twitch.tv:443"),
-	TMI("http://tmi.twitch.tv");
+	TMI("http://tmi.twitch.tv"),
+	OAUTH("https://id.twitch.tv");
 
 	private final String URL;
 
 	Endpoints(String url) {
 		this.URL = url;
+	}
+
+	public String buildUrl(String endpoint) {
+		return String.format("%s/%s", this.URL, endpoint);
 	}
 }
