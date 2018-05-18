e861009862a8f6f10ed94982b1eeeff60683e446
Damian Staszewski <damian@stachuofficial.pl>
2018-05-19 01:16:59 +0200

    feat(UTIL): browser open to multi-platform supportive

--- src/main/java/me/philippheuer/util/desktop/WebsiteUtils.java
+++ src/main/java/me/philippheuer/util/desktop/WebsiteUtils.java
@@ -1,5 +1,8 @@
 package me.philippheuer.util.desktop;
 
+import com.jcabi.log.Logger;
+import org.apache.commons.lang3.SystemUtils;
+
 import java.awt.*;
 import java.net.URI;
 
@@ -22,9 +25,29 @@ public class WebsiteUtils {
 			Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
 			if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
 				desktop.browse(new URI(uri));
+			} else {
+				throw new UnsupportedOperationException("Desktop class can't open browser, trying open from Runtime.");
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			Logger.warn(WebsiteUtils.class, e.getMessage(), e);
+			openRuntime(uri);
+		}
+	}
+
+	private static void openRuntime(String uri) {
+		Runtime runtime = Runtime.getRuntime();
+		try {
+			if (SystemUtils.IS_OS_WINDOWS) {
+				runtime.exec("rundll32 url.dll,FileProtocolHandler " + uri);
+			} else if (SystemUtils.IS_OS_MAC) {
+				runtime.exec("open " + uri);
+			} else if (SystemUtils.IS_OS_LINUX) {
+				runtime.exec("xdg-open "+ uri);
+			} else {
+				throw new RuntimeException("Cannot open url: " + uri, new UnsupportedOperationException("You OS is unsupported"));
+			}
+		} catch (Exception ex) {
+			Logger.error(WebsiteUtils.class, ex.getMessage(), ex);
 		}
 	}
 
