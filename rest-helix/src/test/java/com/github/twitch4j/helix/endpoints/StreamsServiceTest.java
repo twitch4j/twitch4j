53b096e77a29f75c8f4530c62477f62c2f4c91bd
Philipp Heuer <git@philippheuer.me>
2019-04-06 15:35:59 +0200

    chore: add docs and tests for streamTags

--- rest-helix/src/test/java/com/github/twitch4j/helix/endpoints/StreamsServiceTest.java
+++ rest-helix/src/test/java/com/github/twitch4j/helix/endpoints/StreamsServiceTest.java
@@ -1,16 +1,16 @@
 package com.github.twitch4j.helix.endpoints;
 
-import com.github.twitch4j.helix.domain.StreamList;
-import com.github.twitch4j.helix.domain.StreamMarkersList;
-import com.github.twitch4j.helix.domain.StreamMetadata;
-import com.github.twitch4j.helix.domain.StreamMetadataList;
+import com.github.twitch4j.helix.domain.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -18,6 +18,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @Slf4j
 @Tag("integration")
 public class StreamsServiceTest extends AbtractEndpointTest {
+
+    // UserId
+    private static Long twitchUserId = 149223493L;
 
     // Hearthstone GameId
     private static String hearthstoneGameId = "138585";
@@ -103,5 +106,55 @@ public class StreamsServiceTest extends AbtractEndpointTest {
             });
         });
     }
+
+    /**
+     * getAllStreamTags
+     */
+    @Test
+    @DisplayName("getAllStreamTags")
+    public void getAllStreamTags() {
+        // TestCase
+        StreamTagList resultList = testUtils.getTwitchHelixClient().getAllStreamTags(null, "", 100, null).execute();
+
+        // Test
+        assertTrue(resultList.getStreamTags().size() > 0, "Should at least find one stream tag!");
+        resultList.getStreamTags().forEach(tag -> {
+            assertTrue(tag.getTagId() != null, "TagId should not be null");
+        });
+    }
+
+    /**
+     * Gets stream tags which are active on the specified stream.
+     */
+    @Test
+    @DisplayName("getStreamTagsOfStream")
+    @Disabled
+    public void getStreamTagsOfStream() {
+        // TestCase
+        StreamTagList resultList = testUtils.getTwitchHelixClient().getStreamTags(null, twitchUserId).execute();
+
+        // Test
+        assertTrue(resultList.getStreamTags().size() == 1, "Should have exactly 1 stream tag!");
+        resultList.getStreamTags().forEach(tag -> {
+            assertTrue(tag.getTagId().equals(UUID.fromString("a59f1e4e-257b-4bd0-90c7-189c3efbf917")), "TagId doesn't match a59f1e4e-257b-4bd0-90c7-189c3efbf917");
+            assertTrue(tag.getLocalizationNames().get("en-us").equalsIgnoreCase("programming"), "tag en-us name should equal programming!");
+            assertTrue(tag.getLocalizationDescriptions().get("en-us").equalsIgnoreCase("For streams with an emphasis on the discussion or process of computer programming"), "tag en-us description doesn't match!");
+        });
+    }
+
+    /**
+     * replaceStreamTags
+     */
+    @Test
+    @DisplayName("replaceStreamTags")
+    @Disabled
+    public void replaceStreamTags() {
+        // TestCase
+        List<UUID> tagIds = new ArrayList<>();
+        tagIds.add(UUID.fromString("a59f1e4e-257b-4bd0-90c7-189c3efbf917"));
+
+        testUtils.getTwitchHelixClient().replaceStreamTags(testUtils.getCredential().getAccessToken(), twitchUserId, tagIds).execute();
+    }
+
 }
 
