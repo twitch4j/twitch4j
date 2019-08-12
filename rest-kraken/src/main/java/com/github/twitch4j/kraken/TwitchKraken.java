1a8f64df77e8209e11936db3d25804e70478b1d4
lclc98 <liam.c14785@gmail.com>
2019-08-12 18:42:10 +1000

    Fixed request to support multiple logins

--- rest-kraken/src/main/java/com/github/twitch4j/kraken/TwitchKraken.java
+++ rest-kraken/src/main/java/com/github/twitch4j/kraken/TwitchKraken.java
@@ -6,9 +6,9 @@ import com.github.twitch4j.kraken.domain.KrakenIngestList;
 import com.github.twitch4j.kraken.domain.KrakenSubscriptionList;
 import com.github.twitch4j.kraken.domain.KrakenTeam;
 import com.github.twitch4j.kraken.domain.KrakenTeamList;
-import com.github.twitch4j.kraken.domain.KrakenUser;
 import com.github.twitch4j.kraken.domain.KrakenUserList;
 import com.netflix.hystrix.HystrixCommand;
+import feign.CollectionFormat;
 import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
@@ -106,21 +106,21 @@ public interface TwitchKraken {
     HystrixCommand<KrakenTeam> getTeamByName(
         @Param("name") String name
     );
-    
+
     /**
      * Get Users
      * <p>
      * Gets a list of specified user objects.
-     * 
+     *
      * @param logins User login name (lower case channelname/username). Multiple login names can be specified. Limit: 100.
      * @return KrakenUser
      */
-    @RequestLine("GET /users?login={logins}")
+    @RequestLine(value = "GET /users?login={logins}", collectionFormat = CollectionFormat.CSV)
     @Headers({
         "Accept: application/vnd.twitchtv.v5+json"
     })
     HystrixCommand<KrakenUserList> getUsersByLogin(
-    	@Param("logins") List<String> logins	
+    	@Param("logins") List<String> logins
     );
 
 }
