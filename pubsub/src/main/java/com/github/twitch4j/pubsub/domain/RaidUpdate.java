6afa8c4ae3ecc6dbc3dc0d7852d9d5165c9cb1a2
Sidd <iProdigy@users.noreply.github.com>
2020-06-07 01:05:24 -0500

    feat: add objects for pubsub raid event, add more channel point event types

---
+++ pubsub/src/main/java/com/github/twitch4j/pubsub/domain/RaidUpdate.java
@@ -0,0 +1,22 @@
+package com.github.twitch4j.pubsub.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+@Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RaidUpdate {
+    private String id;
+    private String creatorId;
+    private String sourceId;
+    private String targetId;
+    private String targetLogin;
+    private String targetDisplayName;
+    private String targetProfileImage;
+    private Integer transitionJitterSeconds;
+    private Integer forceRaidNowSeconds;
+    private Integer viewerCount;
+}
