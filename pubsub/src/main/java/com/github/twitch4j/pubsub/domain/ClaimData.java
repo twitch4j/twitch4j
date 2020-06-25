3e23492fd0b56616264662e33e4d9d5680f7af94
Sidd <iProdigy@users.noreply.github.com>
2020-06-25 00:16:35 -0500

    refactor: no longer need to manually unpack timestamps

--- pubsub/src/main/java/com/github/twitch4j/pubsub/domain/ClaimData.java
+++ pubsub/src/main/java/com/github/twitch4j/pubsub/domain/ClaimData.java
@@ -1,8 +1,6 @@
 package com.github.twitch4j.pubsub.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
@@ -13,14 +11,8 @@ import java.time.Instant;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ClaimData {
-    @JsonIgnore
     private Instant timestamp;
     private Claim claim;
-
-    @JsonProperty("timestamp")
-    private void unpackTimestamp(final String timestamp) {
-        this.timestamp = Instant.parse(timestamp);
-    }
 
     @Data
     @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
@@ -30,12 +22,6 @@ public class ClaimData {
         private String userId;
         private String channelId;
         private ChannelPointsGain pointGain;
-        @JsonIgnore
         private Instant createdAt;
-
-        @JsonProperty("created_at")
-        private void unpackCreatedAt(final String createdAt) {
-            this.createdAt = Instant.parse(createdAt);
-        }
     }
 }
--- pubsub/src/main/java/com/github/twitch4j/pubsub/domain/PointsSpent.java
+++ pubsub/src/main/java/com/github/twitch4j/pubsub/domain/PointsSpent.java
@@ -1,8 +1,6 @@
 package com.github.twitch4j.pubsub.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
@@ -13,14 +11,8 @@ import java.time.Instant;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PointsSpent {
-    @JsonIgnore
     private Instant timestamp;
     private Balance balance;
-
-    @JsonProperty("timestamp")
-    private void unpackTimestamp(final String timestamp) {
-        this.timestamp = Instant.parse(timestamp);
-    }
 
     @Data
     @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
