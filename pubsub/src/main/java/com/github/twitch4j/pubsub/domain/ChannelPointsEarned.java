f32c698c5dcfd703c74bb463625081bb9a4847e0
Sidd <iProdigy@users.noreply.github.com>
2020-06-25 00:16:35 -0500

    refactor: no longer need to manually unpack timestamps

--- pubsub/src/main/java/com/github/twitch4j/pubsub/domain/ChannelPointsEarned.java
+++ pubsub/src/main/java/com/github/twitch4j/pubsub/domain/ChannelPointsEarned.java
@@ -1,12 +1,8 @@
 package com.github.twitch4j.pubsub.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.github.twitch4j.pubsub.domain.ChannelPointsBalance;
-import com.github.twitch4j.pubsub.domain.ChannelPointsGain;
 import lombok.Data;
 
 import java.time.Instant;
@@ -15,14 +11,8 @@ import java.time.Instant;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ChannelPointsEarned {
-    @JsonIgnore
     private Instant timestamp;
     private String channelId;
     private ChannelPointsGain pointGain;
     private ChannelPointsBalance balance;
-
-    @JsonProperty("timestamp")
-    private void unpackTimestamp(final String timestamp) {
-        this.timestamp = Instant.parse(timestamp);
-    }
 }
--- pubsub/src/main/java/com/github/twitch4j/pubsub/domain/HypeLevelUp.java
+++ pubsub/src/main/java/com/github/twitch4j/pubsub/domain/HypeLevelUp.java
@@ -1,8 +1,6 @@
 package com.github.twitch4j.pubsub.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
@@ -13,13 +11,6 @@ import java.time.Instant;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class HypeLevelUp {
-    @JsonIgnore
     private Instant timeToExpire;
     private HypeTrainProgress progress;
-
-    @JsonProperty("time_to_expire")
-    private void unpackTimeToExpire(final Long timeToExpire) {
-        if (timeToExpire != null)
-            this.timeToExpire = Instant.ofEpochMilli(timeToExpire);
-    }
 }
