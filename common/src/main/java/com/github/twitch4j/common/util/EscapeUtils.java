06e08da805031f181cdb6b6ecabc557d6a6a5818
Sidd <iProdigy@users.noreply.github.com>
2020-08-29 17:55:13 -0400

    perf: optimize escapeTagValue for no replacements case

--- common/src/main/java/com/github/twitch4j/common/util/EscapeUtils.java
+++ common/src/main/java/com/github/twitch4j/common/util/EscapeUtils.java
@@ -12,10 +12,30 @@ public class EscapeUtils {
      * @see <a href="https://ircv3.net/specs/extensions/message-tags.html">Offical spec</a>
      */
     public static String escapeTagValue(Object value) {
-        if (value == null) return "";
-        char[] unescaped = value.toString().toCharArray();
-        StringBuilder sb = new StringBuilder(unescaped.length);
-        for (char c : unescaped) {
+        final String unescapedString;
+        if (value == null || (unescapedString = value.toString()) == null) return "";
+
+        final char[] unescaped = unescapedString.toCharArray();
+        final int n = unescaped.length;
+
+        // Determine the index of the first replacement needed in the string
+        int firstReplacement = -1;
+        for (int i = 0; i < n; i++) {
+            char c = unescaped[i];
+            if (c == ';' || c == ' ' || c == '\\' || c == '\r' || c == '\n') {
+                firstReplacement = i;
+                break;
+            }
+        }
+
+        // When no replacements are needed, skip allocating the StringBuilder and copying over the chars
+        if (firstReplacement < 0) return unescapedString;
+
+        // Otherwise: replacement(s) are needed
+        final StringBuilder sb = new StringBuilder(n + 1); // Set capacity to length of string plus one replacement
+        sb.append(unescaped, 0, firstReplacement); // Copy over the region of the string that requires no replacements
+        for (int i = firstReplacement; i < n; i++) { // Perform replacements on the rest of the string as needed
+            char c = unescaped[i];
             switch (c) {
                 case ';':
                     sb.append("\\:");
