a7fca07c0496c8d2ea3316a9c243357eac24b42e
Sidd <iProdigy@users.noreply.github.com>
2020-09-14 17:51:34 -0500

    fix: deserialize pagination in extension transaction list

--- rest-helix/src/main/java/com/github/twitch4j/helix/domain/ExtensionTransactionList.java
+++ rest-helix/src/main/java/com/github/twitch4j/helix/domain/ExtensionTransactionList.java
@@ -2,8 +2,6 @@ package com.github.twitch4j.helix.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,20 +12,18 @@ import java.util.List;
 @Data
 @Setter(AccessLevel.PRIVATE)
 @NoArgsConstructor
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ExtensionTransactionList {
-	
-	/**
+
+    /**
      * Array of requested transactions.
-	 */
-	@JsonProperty("data")
-	private List<ExtensionTransaction> transactions;
-	
-	/**
+     */
+    @JsonProperty("data")
+    private List<ExtensionTransaction> transactions;
+
+    /**
      * If provided, is the key used to fetch the next page of data. If not provided, the current response is the last page of data available.
-	 */
-	@JsonProperty("pagination")
-	private String pagination;
-	
+     */
+    private HelixPagination pagination;
+
 }
