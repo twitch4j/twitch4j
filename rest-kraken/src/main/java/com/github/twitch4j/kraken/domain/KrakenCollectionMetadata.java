cd4e0e2327824b350af8ac4d5e47eef7e79e25fa
p4czyk <56545204+p4czyk@users.noreply.github.com>
2021-01-16 13:19:35 +0100

    refactor: add SnakeCaseStrategy to KrakenCollectionItem and KrakenCollectionMetadata

--- rest-kraken/src/main/java/com/github/twitch4j/kraken/domain/KrakenCollectionMetadata.java
+++ rest-kraken/src/main/java/com/github/twitch4j/kraken/domain/KrakenCollectionMetadata.java
@@ -2,6 +2,8 @@ package com.github.twitch4j.kraken.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
@@ -11,28 +13,17 @@ import java.time.Instant;
 @Data
 @Setter(AccessLevel.PRIVATE)
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class KrakenCollectionMetadata {
 
     @JsonProperty("_id")
     private String id;
-
-    @JsonProperty("created_at")
     private Instant createdAt;
-
-    @JsonProperty("items_count")
     private Integer itemsCount;
-
     private KrakenUser owner;
-
     private KrakenCollectionThumbnails thumbnails;
-
     private String title;
-
-    @JsonProperty("total_duration")
     private Integer totalDuration;
-
-    @JsonProperty("updated_at")
     private Instant updatedAt;
-
     private Integer views;
 }
