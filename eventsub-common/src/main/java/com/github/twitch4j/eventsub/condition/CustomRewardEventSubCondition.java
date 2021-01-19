00c9147420715b086620398e0c474da88a970fc9
Sidd <iProdigy@users.noreply.github.com>
2021-01-18 20:06:23 -0600

    refactor: remove obsolete EventSubCondition#toMap (#256)

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/condition/CustomRewardEventSubCondition.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/condition/CustomRewardEventSubCondition.java
@@ -11,10 +11,6 @@ import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 @Data
 @Setter(AccessLevel.PRIVATE)
 @SuperBuilder
@@ -29,14 +25,5 @@ public class CustomRewardEventSubCondition extends ChannelEventSubCondition {
      * Specify a reward id to only receive notifications for a specific reward.
      */
     private String rewardId;
-
-    @Override
-    public Map<String, Object> toMap() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("broadcaster_user_id", getBroadcasterUserId());
-        if (rewardId != null)
-            map.put("reward_id", this.rewardId);
-        return Collections.unmodifiableMap(map);
-    }
 
 }
--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/condition/UserEventSubCondition.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/condition/UserEventSubCondition.java
@@ -10,9 +10,6 @@ import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
 
-import java.util.Collections;
-import java.util.Map;
-
 @Data
 @Setter(AccessLevel.PRIVATE)
 @SuperBuilder
@@ -26,10 +23,5 @@ public class UserEventSubCondition extends EventSubCondition {
      * The user ID for the user you want notifications for.
      */
     private String userId;
-
-    @Override
-    public Map<String, Object> toMap() {
-        return Collections.singletonMap("user_id", this.userId);
-    }
 
 }
