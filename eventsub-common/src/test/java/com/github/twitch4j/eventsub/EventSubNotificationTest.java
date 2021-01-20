8df4a5ef4c26086031001123b85dc78ac4a6b1ad
Sidd <iProdigy@users.noreply.github.com>
2021-01-20 15:59:15 -0600

    feat: include challenge in EventSubNotification

--- eventsub-common/src/test/java/com/github/twitch4j/eventsub/EventSubNotificationTest.java
+++ eventsub-common/src/test/java/com/github/twitch4j/eventsub/EventSubNotificationTest.java
@@ -4,6 +4,7 @@ import com.github.twitch4j.common.util.TypeConvert;
 import com.github.twitch4j.eventsub.condition.ChannelFollowCondition;
 import com.github.twitch4j.eventsub.events.ChannelFollowEvent;
 import com.github.twitch4j.eventsub.subscriptions.SubscriptionTypes;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +18,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class EventSubNotificationTest {
 
     @Test
+    @DisplayName("Deserialize Challenge")
+    public void deserializeChallenge() {
+        String json = "{\"subscription\":{\"id\":\"b9ee086b-4ff1-4f11-8be8-9c93e3cd9515\",\"status\":\"webhook_callback_verification_pending\",\"type\":\"channel.follow\",\"version\":\"1\",\"condition\":{\"broadcaster_user_id\":\"207813352\"}," +
+            "\"transport\":{\"method\":\"webhook\",\"callback\":\"https://twitch4j.ngrok.io\"},\"created_at\":\"2021-01-20T19:41:07.513199878Z\"},\"challenge\":\"sample-challenge\"}";
+
+        EventSubNotification notif = TypeConvert.jsonToObject(json, EventSubNotification.class);
+        assertEquals("sample-challenge", notif.getChallenge());
+    }
+
+    @Test
+    @DisplayName("Deserialize Follow Notification")
     public void deserializeFollowNotification() {
         EventSubNotification notif = TypeConvert.jsonToObject(
             "{\"subscription\":{\"id\":\"f1c2a387-161a-49f9-a165-0f21d7a4e1c4\",\"status\":\"authorization_revoked\",\"type\":\"channel.follow\",\"version\":\"1\",\"condition\":{\"broadcaster_user_id\":\"12826\"},\"transport\":{\"method\":\"webhook\"," +
