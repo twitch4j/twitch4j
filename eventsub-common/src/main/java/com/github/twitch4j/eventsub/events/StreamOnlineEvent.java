36226b87f73f189cc5fe417e0d0d343d5c14ac9b
Sidd <iProdigy@users.noreply.github.com>
2021-01-30 23:45:05 -0600

    feat: include new started_at field for eventsub StreamOnlineEvent

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/events/StreamOnlineEvent.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/events/StreamOnlineEvent.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.Instant;
+
 @Data
 @Setter(AccessLevel.PRIVATE)
 @NoArgsConstructor
@@ -24,5 +26,10 @@ public class StreamOnlineEvent extends EventSubChannelEvent {
      * The stream type.
      */
     private StreamType type;
+
+    /**
+     * The timestamp at which the stream went online at.
+     */
+    private Instant startedAt;
 
 }
