9eb1012cdc2bfc82f98ea44287daf4f2671733cf
Sidd <iProdigy@users.noreply.github.com>
2021-06-02 16:33:28 -0500

    feat: include new total field in helix SubscriptionList

--- rest-helix/src/main/java/com/github/twitch4j/helix/domain/SubscriptionList.java
+++ rest-helix/src/main/java/com/github/twitch4j/helix/domain/SubscriptionList.java
@@ -7,9 +7,21 @@ import java.util.List;
 
 @Data
 public class SubscriptionList {
+
+    /**
+     * The subscriptions.
+     */
     @JsonProperty("data")
     private List<Subscription> subscriptions;
 
+    /**
+     * A cursor value, to be used in a subsequent request to specify the starting point of the next set of results. If this is empty, you are at the last page.
+     */
     private HelixPagination pagination;
+
+    /**
+     * The number of Twitch users subscribed to the broadcaster.
+     */
+    private Integer total;
 
 }
