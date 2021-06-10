7daf45e7845b6405c91d16933f8f9636389b2445
Sidd <iProdigy@users.noreply.github.com>
2021-06-10 15:04:06 -0500

    feat: parse notice msgId to convenient enum (#377)

--- chat/src/main/java/com/github/twitch4j/chat/events/channel/ChannelNoticeEvent.java
+++ chat/src/main/java/com/github/twitch4j/chat/events/channel/ChannelNoticeEvent.java
@@ -1,39 +1,49 @@
 package com.github.twitch4j.chat.events.channel;
 
+import com.github.twitch4j.chat.enums.NoticeTag;
 import com.github.twitch4j.chat.events.AbstractChannelEvent;
 import com.github.twitch4j.common.events.domain.EventChannel;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
+import lombok.ToString;
 import lombok.Value;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * ChatChannel Notice Event
  */
 @Value
-@Getter
-@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class ChannelNoticeEvent extends AbstractChannelEvent {
 
     /**
      * Message Id
      */
-	private final String msgId;
+    private final String msgId;
 
     /**
      * Message Content
      */
-	private final String message;
+    private final String message;
+
+    /**
+     * The parsed value of {@link #getMsgId()}.
+     */
+    @Nullable
+    NoticeTag type;
 
     /**
      * Channel Notice Event
      *
      * @param channel ChatChannel
-     * @param msgId Message Id
+     * @param msgId   Message Id
      * @param message message Content
      */
-	public ChannelNoticeEvent(EventChannel channel, String msgId, String message) {
-		super(channel);
-		this.msgId = msgId;
-		this.message = message;
-	}
+    public ChannelNoticeEvent(EventChannel channel, String msgId, String message) {
+        super(channel);
+        this.msgId = msgId;
+        this.type = NoticeTag.parse(msgId);
+        this.message = message;
+    }
+
 }
