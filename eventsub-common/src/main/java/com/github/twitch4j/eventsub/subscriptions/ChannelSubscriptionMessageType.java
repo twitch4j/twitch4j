0fc45b1dad43283507c27b20608f6d4a406decca
Sidd <iProdigy@users.noreply.github.com>
2021-06-15 20:53:39 -0500

    chore: promote sub message and gift from beta to v1

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/subscriptions/ChannelSubscriptionMessageType.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/subscriptions/ChannelSubscriptionMessageType.java
@@ -1,6 +1,5 @@
 package com.github.twitch4j.eventsub.subscriptions;
 
-import com.github.twitch4j.common.annotation.Unofficial;
 import com.github.twitch4j.eventsub.condition.ChannelSubscriptionMessageCondition;
 import com.github.twitch4j.eventsub.events.ChannelSubscriptionMessageEvent;
 
@@ -8,12 +7,8 @@ import com.github.twitch4j.eventsub.events.ChannelSubscriptionMessageEvent;
  * The channel.subscription.message subscription type sends a notification when a user sends a re-subscription chat message in a specific channel.
  * <p>
  * Must have channel:read:subscriptions scope.
- * <p>
- * Unless otherwise noted, EventSub subscriptions that were released as a public beta will be available for 30 days after their v1 version is released.
- * Subscriptions should be updated to v1 during this timeframe. Any active beta subscriptions beyond 30 days will be automatically deleted.
  */
-@Unofficial
-public class BetaChannelSubscriptionMessageType implements SubscriptionType<ChannelSubscriptionMessageCondition, ChannelSubscriptionMessageCondition.ChannelSubscriptionMessageConditionBuilder<?, ?>, ChannelSubscriptionMessageEvent> {
+public class ChannelSubscriptionMessageType implements SubscriptionType<ChannelSubscriptionMessageCondition, ChannelSubscriptionMessageCondition.ChannelSubscriptionMessageConditionBuilder<?, ?>, ChannelSubscriptionMessageEvent> {
 
     @Override
     public String getName() {
@@ -22,7 +17,7 @@ public class BetaChannelSubscriptionMessageType implements SubscriptionType<Chan
 
     @Override
     public String getVersion() {
-        return "beta";
+        return "1";
     }
 
     @Override
