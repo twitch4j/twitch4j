69856f7ae5409738648945a40097499a7c8d141f
Sidd <iProdigy@users.noreply.github.com>
2021-06-19 00:15:52 -0500

    feat: add new fields in unsubscribe event

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/events/ChannelUnsubscribeEvent.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/events/ChannelUnsubscribeEvent.java
@@ -1,8 +1,32 @@
 package com.github.twitch4j.eventsub.events;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.twitch4j.common.enums.SubscriptionPlan;
+import lombok.AccessLevel;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 
+@Data
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class ChannelUnsubscribeEvent extends EventSubUserChannelEvent {}
+public class ChannelUnsubscribeEvent extends EventSubUserChannelEvent {
+
+    /**
+     * The tier of the subscription that ended.
+     */
+    private SubscriptionPlan tier;
+
+    /**
+     * Whether the subscription was a gift.
+     */
+    @Accessors(fluent = true)
+    @JsonProperty("is_gift")
+    private Boolean isGift;
+
+}
