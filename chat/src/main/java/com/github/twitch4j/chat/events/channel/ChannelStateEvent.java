5e914efac183cfa0bddcf685a3d5e96446ad25d4
Sidd <iProdigy@users.noreply.github.com>
2021-10-26 17:46:13 -0500

    feat: include rituals and deprecate lang in ChannelStateEvent (#428)

--- chat/src/main/java/com/github/twitch4j/chat/events/channel/ChannelStateEvent.java
+++ chat/src/main/java/com/github/twitch4j/chat/events/channel/ChannelStateEvent.java
@@ -1,6 +1,7 @@
 package com.github.twitch4j.chat.events.channel;
 
 import com.github.twitch4j.chat.events.AbstractChannelEvent;
+import com.github.twitch4j.common.annotation.Unofficial;
 import com.github.twitch4j.common.events.domain.EventChannel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -14,39 +15,42 @@ import java.util.Map;
 @Getter
 @EqualsAndHashCode(callSuper = false)
 public class ChannelStateEvent extends AbstractChannelEvent {
-	public enum ChannelState {
-		BROADCAST_LANG,
-		EMOTE,
-		FOLLOWERS,
-		R9K,
-		SLOW,
-		SUBSCRIBERS;
-	}
+    public enum ChannelState {
+        @Deprecated
+        BROADCAST_LANG,
+        EMOTE,
+        FOLLOWERS,
+        R9K,
+        @Unofficial
+        RITUALS,
+        SLOW,
+        SUBSCRIBERS;
+    }
 
-	private final Map<ChannelState, Object> states;
+    private final Map<ChannelState, Object> states;
 
-	/**
-	 * Event Constructor
-	 *
-	 * @param channel The channel that this event originates from.
-     * @param state The changed state triggering the event
-     * @param value The value representing the state
-	 */
-	public ChannelStateEvent(EventChannel channel, ChannelState state, Object value) {
-		super(channel);
-		Map<ChannelState, Object> states = new HashMap<>();
-		states.put(state, value);
-		this.states = Collections.unmodifiableMap(states);
-	}
+    /**
+     * Event Constructor
+     *
+     * @param channel The channel that this event originates from.
+     * @param state   The changed state triggering the event
+     * @param value   The value representing the state
+     */
+    public ChannelStateEvent(EventChannel channel, ChannelState state, Object value) {
+        super(channel);
+        Map<ChannelState, Object> states = new HashMap<>();
+        states.put(state, value);
+        this.states = Collections.unmodifiableMap(states);
+    }
 
-	public ChannelStateEvent(EventChannel channel, Map<ChannelState, Object> state) {
-		super(channel);
-		Map<ChannelState, Object> states = new HashMap<>();
-		states.putAll(state);
-		this.states = Collections.unmodifiableMap(states);
-	}
+    public ChannelStateEvent(EventChannel channel, Map<ChannelState, Object> state) {
+        super(channel);
+        Map<ChannelState, Object> states = new HashMap<>(state);
+        this.states = Collections.unmodifiableMap(states);
+    }
 
-	public Object getState(ChannelState state) {
-		return states.getOrDefault(state, null);
-	}
+    public Object getState(ChannelState state) {
+        return states.getOrDefault(state, null);
+    }
+
 }
