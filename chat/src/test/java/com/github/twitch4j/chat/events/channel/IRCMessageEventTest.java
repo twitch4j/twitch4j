494af671685d2cc32f707f518631e7c0bda6b21b
Sidd <iProdigy@users.noreply.github.com>
2022-07-30 15:12:34 -0500

    chore: add unit test for globaluserstate parsing

--- chat/src/test/java/com/github/twitch4j/chat/events/channel/IRCMessageEventTest.java
+++ chat/src/test/java/com/github/twitch4j/chat/events/channel/IRCMessageEventTest.java
@@ -36,6 +36,18 @@ public class IRCMessageEventTest {
     }
 
     @Test
+    @DisplayName("Tests that GLOBALUSERSTATE is parsed by IRCMessageEvent")
+    void parseGlobalUserState() {
+        IRCMessageEvent e = build("@badge-info=subscriber/8;badges=subscriber/6;color=#0D4200;display-name=dallas;emote-sets=0,33,50,237,793,2126,3517,4578,5569,9400,10337,12239;turbo=0;user-id=12345678;user-type=admin " +
+            ":tmi.twitch.tv GLOBALUSERSTATE");
+
+        assertEquals("GLOBALUSERSTATE", e.getCommandType());
+        assertEquals("12345678", e.getUserId());
+        assertEquals("dallas", e.getTagValue("display-name").orElse(null));
+        assertEquals("0,33,50,237,793,2126,3517,4578,5569,9400,10337,12239", e.getTagValue("emote-sets").orElse(null));
+    }
+
+    @Test
     @DisplayName("Test that normal messages are parsed by IRCMessageEvent")
     void parseMessage() {
         IRCMessageEvent e = build("@badge-info=;badges=broadcaster/1;client-nonce=459e3142897c7a22b7d275178f2259e0;color=#0000FF;display-name=lovingt3s;emote-only=1;emotes=62835:0-10;first-msg=0;flags=;" +
