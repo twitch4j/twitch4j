f36744989d8b144a34a34e7466114101523251cf
iProdigy <iProdigy@users.noreply.github.com>
2022-08-09 16:43:15 -0500

    fix: deserialize eventsub follow goal type (#620)
    * fix: deserialize eventsub follow goal type due to twitch doc issue
    
    * chore: define more goal type aliases to be safe

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/domain/GoalType.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/domain/GoalType.java
@@ -10,21 +10,21 @@ public enum GoalType {
     /**
      * The goal is to increase followers.
      */
-    @JsonAlias("follower")
+    @JsonAlias({ "follower", "follow" })
     FOLLOWERS,
 
     /**
      * The goal is to increase subscriptions.
      * This type shows the net increase or decrease in subscriptions.
      */
-    @JsonAlias("subscription")
+    @JsonAlias({ "subscription", "sub" })
     SUBSCRIPTIONS,
 
     /**
      * The goal is to increase subscriptions.
      * This type shows only the net increase in subscriptions (it does not account for users that stopped subscribing since the goal's inception).
      */
-    @JsonAlias("new_subscription")
+    @JsonAlias({ "new_subscription", "new_sub" })
     NEW_SUBSCRIPTIONS,
 
     /**
