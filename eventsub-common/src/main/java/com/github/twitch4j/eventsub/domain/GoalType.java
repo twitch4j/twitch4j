39e0ee079f98980ff8e9f4fe6f6b523284c86b29
Sidd <iProdigy@users.noreply.github.com>
2022-08-09 13:25:47 -0500

    chore: define more goal type aliases to be safe

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/domain/GoalType.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/domain/GoalType.java
@@ -17,14 +17,14 @@ public enum GoalType {
      * The goal is to increase subscriptions.
      * This type shows the net increase or decrease in subscriptions.
      */
-    @JsonAlias("subscription")
+    @JsonAlias({ "subscription", "sub" })
     SUBSCRIPTIONS,
 
     /**
      * The goal is to increase subscriptions.
      * This type shows only the net increase in subscriptions (it does not account for users that stopped subscribing since the goal's inception).
      */
-    @JsonAlias("new_subscription")
+    @JsonAlias({ "new_subscription", "new_sub" })
     NEW_SUBSCRIPTIONS,
 
     /**
