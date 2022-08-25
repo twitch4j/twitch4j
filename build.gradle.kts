10ce56eb9e6733dc197a14aaf4b5e751bb271ccb
iProdigy <iProdigy@users.noreply.github.com>
2022-08-25 12:15:02 -0500

    fix: prevent maven from using jackson3 snapshot (#625)
    * fix: prevent maven from using jackson3 snapshot
    
    * chore: allow renovate to update the jackson version
    
    Co-authored-by: Philipp Heuer <git@philippheuer.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -110,8 +110,9 @@ subprojects {
 			listOf("com.fasterxml.jackson.core:jackson-annotations", "com.fasterxml.jackson.core:jackson-core", "com.fasterxml.jackson.core:jackson-databind", "com.fasterxml.jackson.datatype:jackson-datatype-jsr310").forEach { dep ->
 				add("api", dep) {
 					version {
-						strictly("[2.12,3[")
-						prefer("2.13.2")
+						strictly("[2.12,3-alpha[")
+						// renovate: depName=com.fasterxml.jackson:jackson-bom
+						prefer("2.13.3")
 					}
 				}
 			}
