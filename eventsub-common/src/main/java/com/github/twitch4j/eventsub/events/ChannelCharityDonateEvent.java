81b30ccec8af0253ef62a14d763923d01c8091c0
Sidd <iProdigy@users.noreply.github.com>
2022-08-26 00:14:44 -0700

    feat(eventsub): add charity campaign donate type

---
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/events/ChannelCharityDonateEvent.java
@@ -0,0 +1,30 @@
+package com.github.twitch4j.eventsub.events;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.twitch4j.common.util.DonationAmount;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class ChannelCharityDonateEvent extends EventSubUserChannelEvent {
+
+    /**
+     * An ID that uniquely identifies the charity campaign.
+     */
+    @JsonProperty("id")
+    private String campaignId;
+
+    /**
+     * An object that contains the amount of the user’s donation.
+     */
+    private DonationAmount amount;
+
+}
