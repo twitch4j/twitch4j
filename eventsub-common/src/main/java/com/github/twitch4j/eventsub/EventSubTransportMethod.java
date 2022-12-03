a6fc52ed6fba9ebbf162e752cfecdc85e60a8259
Sidd <gitProdigy@proton.me>
2022-12-02 20:33:09 -0800

    fix: serialize transport method as lowercase

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/EventSubTransportMethod.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/EventSubTransportMethod.java
@@ -1,7 +1,11 @@
 package com.github.twitch4j.eventsub;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum EventSubTransportMethod {
+    @JsonProperty("webhook")
     WEBHOOK,
+    @JsonProperty("websocket")
     WEBSOCKET;
 
     @Override
