d6bc96f234656c329a27febb6d486d8187e8b642
Sidd <gitProdigy@proton.me>
2023-02-15 18:29:01 -0800

    fix: doubly ensure response is closed

--- rest-helix/src/main/java/com/github/twitch4j/helix/TwitchHelixErrorDecoder.java
+++ rest-helix/src/main/java/com/github/twitch4j/helix/TwitchHelixErrorDecoder.java
@@ -16,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.exception.ContextedRuntimeException;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
@@ -101,8 +100,10 @@ public class TwitchHelixErrorDecoder implements ErrorDecoder {
                     .addContextValue("errorType", error.getMessage())
                     .addContextValue("errorMessage", error.getMessage());
             }
-        } catch (IOException fallbackToDefault) {
+        } catch (Exception fallbackToDefault) {
             ex = defaultDecoder.decode(methodKey, response);
+        } finally {
+            response.close();
         }
 
         return ex;
