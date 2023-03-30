bc87192455966372aad6d9be197a882616870e8e
Sidd <gitProdigy@proton.me>
2023-03-30 11:16:24 -0700

    fix(chat): fire join events from 353 names list

--- chat/src/main/java/com/github/twitch4j/chat/events/channel/ChannelJoinEvent.java
+++ chat/src/main/java/com/github/twitch4j/chat/events/channel/ChannelJoinEvent.java
@@ -4,30 +4,28 @@ import com.github.twitch4j.chat.events.AbstractChannelEvent;
 import com.github.twitch4j.common.events.domain.EventChannel;
 import com.github.twitch4j.common.events.domain.EventUser;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.Value;
 
 /**
- * This event gets called when a client joins a channel.
+ * This event gets called when a user joins a channel.
  */
 @Value
-@Getter
-@EqualsAndHashCode(callSuper = false)
+@EqualsAndHashCode(callSuper = true)
 public class ChannelJoinEvent extends AbstractChannelEvent {
 
-	/**
-	 * User
-	 */
-	private EventUser user;
+    /**
+     * User
+     */
+    EventUser user;
 
-	/**
-	 * Event Constructor
-	 *
-	 * @param channel     The channel that this event originates from.
-	 * @param user        The user triggering the event.
-	 */
-	public ChannelJoinEvent(EventChannel channel, EventUser user) {
-		super(channel);
-		this.user = user;
-	}
+    /**
+     * Event Constructor
+     *
+     * @param channel The channel that this event originates from.
+     * @param user    The user triggering the event.
+     */
+    public ChannelJoinEvent(EventChannel channel, EventUser user) {
+        super(channel);
+        this.user = user;
+    }
 }
