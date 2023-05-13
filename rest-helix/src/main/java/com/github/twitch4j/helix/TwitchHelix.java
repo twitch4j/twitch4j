36d65e0c77b2bfa4de0f367336f2091bb1983bdc
iProdigy <gitProdigy@proton.me>
2023-05-13 01:39:57 -0700

    feat: add TwitchHelix#updateGuestStarSlot

--- rest-helix/src/main/java/com/github/twitch4j/helix/TwitchHelix.java
+++ rest-helix/src/main/java/com/github/twitch4j/helix/TwitchHelix.java
@@ -1607,6 +1607,31 @@ public interface TwitchHelix {
     );
 
     /**
+     * Allows a user to update the assigned slot for a particular user within the active Guest Star session.
+     *
+     * @param authToken         User access token (scope: channel:manage:guest_star or moderator:manage:guest_star) from the broadcaster or a Guest Star moderator.
+     * @param broadcasterId     Required: The ID of the broadcaster running the Guest Star session.
+     * @param moderatorId       Required: The ID of the broadcaster or a user that has permission to moderate the broadcaster’s chat room. This ID must match the user_id in the user access token.
+     * @param sessionId         Required: The ID of the Guest Star session in which to update slot settings.
+     * @param sourceSlotId      Required: The slot assignment previously assigned to a user.
+     * @param destinationSlotId Optional: The slot to move this user assignment to. If the destination slot is occupied, the user assigned will be swapped into source_slot_id.
+     * @return 204 No Content upon a successful request
+     * @see com.github.twitch4j.auth.domain.TwitchScopes#HELIX_CHANNEL_GUEST_STAR_MANAGE
+     * @see com.github.twitch4j.auth.domain.TwitchScopes#HELIX_GUEST_STAR_MANAGE
+     */
+    @ApiStatus.Experimental // in open beta
+    @RequestLine("PATCH /guest_star/slot?broadcaster_id={broadcaster_id}&moderator_id={moderator_id}&session_id={session_id}&source_slot_id={source_slot_id}&destination_slot_id={destination_slot_id}")
+    @Headers("Authorization: Bearer {token}")
+    HystrixCommand<Void> updateGuestStarSlot(
+        @Param("token") String authToken,
+        @Param("broadcaster_id") String broadcasterId,
+        @Param("moderator_id") String moderatorId,
+        @Param("session_id") String sessionId,
+        @Param("source_slot_id") String sourceSlotId,
+        @Param("destination_slot_id") @Nullable String destinationSlotId
+    );
+
+    /**
      * Gets the information of the most recent Hype Train of the given channel ID.
      * After 5 days, if no Hype Train has been active, the endpoint will return an empty response.
      *
