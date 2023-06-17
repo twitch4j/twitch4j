19b263cfede12f76df4abd22b9b20b4028d756f9
iProdigy <gitProdigy@proton.me>
2023-06-17 12:49:58 -0500

    docs: explain fields of PubSubAuthRevokeEvent

--- pubsub/src/main/java/com/github/twitch4j/pubsub/events/PubSubAuthRevokeEvent.java
+++ pubsub/src/main/java/com/github/twitch4j/pubsub/events/PubSubAuthRevokeEvent.java
@@ -1,10 +1,13 @@
 package com.github.twitch4j.pubsub.events;
 
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
 import com.github.twitch4j.common.events.TwitchEvent;
 import com.github.twitch4j.pubsub.ITwitchPubSub;
 import com.github.twitch4j.pubsub.domain.PubSubRequest;
+import com.github.twitch4j.pubsub.enums.PubSubType;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -22,9 +25,27 @@ import java.util.Map;
 @Value
 @EqualsAndHashCode(callSuper = false)
 public class PubSubAuthRevokeEvent extends TwitchEvent {
+    /**
+     * The PubSub websocket that was subscribed to these revoked topics.
+     */
     ITwitchPubSub socket;
+
+    /**
+     * Mappings of revoked topic names to the associated {@link PubSubRequest} (LISTEN).
+     * <p>
+     * Warning: Values can be null if you manually call
+     * {@link ITwitchPubSub#listenOnTopic(PubSubRequest)} or
+     * {@link ITwitchPubSub#listenOnTopic(PubSubType, OAuth2Credential, Collection)}
+     * with requests that contain <b>more than</b> one topic in a <i>single</i> {@link PubSubType#LISTEN}.
+     */
+    @NotNull
+    @ApiStatus.Experimental
     Map<@NotNull String, @Nullable PubSubRequest> revokedListensByTopic;
 
+    /**
+     * @return the topics that were revoked (including the user id's)
+     */
+    @NotNull
     public Collection<String> getTopicNames() {
         return revokedListensByTopic.keySet();
     }
