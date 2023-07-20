9d143e5ab991277f9f4b532e720a57495aeb43ed
iProdigy <gitProdigy@proton.me>
2023-07-20 18:29:20 -0500

    refactor: create HypeChatLevel enum

--- chat/src/main/java/com/github/twitch4j/chat/events/channel/ChannelMessageEvent.java
+++ chat/src/main/java/com/github/twitch4j/chat/events/channel/ChannelMessageEvent.java
@@ -4,6 +4,7 @@ import com.github.twitch4j.chat.events.AbstractChannelMessageEvent;
 import com.github.twitch4j.chat.util.ChatCrowdChant;
 import com.github.twitch4j.common.annotation.Unofficial;
 import com.github.twitch4j.common.enums.CommandPermission;
+import com.github.twitch4j.common.enums.HypeChatLevel;
 import com.github.twitch4j.common.events.domain.EventChannel;
 import com.github.twitch4j.common.events.domain.EventUser;
 import com.github.twitch4j.common.util.ChatReply;
@@ -13,12 +14,8 @@ import lombok.Getter;
 import lombok.ToString;
 import lombok.Value;
 import org.apache.commons.lang3.StringUtils;
-import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
 
-import java.time.Duration;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -30,16 +27,6 @@ import java.util.Set;
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
 public class ChannelMessageEvent extends AbstractChannelMessageEvent {
-
-    /**
-     * @see <a href="https://dev.twitch.tv/docs/irc/tags/#privmsg-tags">Documentation</a>
-     */
-    private static final Map<String, Integer> HYPE_CHAT_LEVELS;
-
-    /**
-     * @see <a href="https://help.twitch.tv/s/article/hype-chat-by-twitch">Documentation</a>
-     */
-    private static final Duration[] HYPE_CHAT_DURATION;
 
     /**
      * Information regarding the parent message being replied to, if applicable.
@@ -157,51 +144,13 @@ public class ChannelMessageEvent extends AbstractChannelMessageEvent {
     }
 
     /**
-     * @return the level associated with the hype chat contribution (determines how long the paid message is pinned).
+     * @return the {@link HypeChatLevel} associated with the hype chat contribution.
      * @see #getElevatedChatPayment()
      */
-    public Optional<Integer> getHypeChatLevel() {
+    public Optional<HypeChatLevel> getHypeChatLevel() {
         return getMessageEvent()
             .getTagValue("pinned-chat-paid-level")
-            .map(HYPE_CHAT_LEVELS::get);
+            .map(HypeChatLevel.MAPPINGS::get);
     }
 
-    /**
-     * @return the amount of time this hype chat message is pinned to the top of chat.
-     * @see #getHypeChatLevel()
-     */
-    @ApiStatus.Experimental // in case twitch allows channels to modify the default durations
-    public Optional<Duration> getHypeChatPinDuration() {
-        return getHypeChatLevel()
-            .filter(i -> i >= 1 && i <= HYPE_CHAT_DURATION.length)
-            .map(i -> HYPE_CHAT_DURATION[i - 1]);
-    }
-
-    static {
-        Map<String, Integer> levels = new HashMap<>();
-        levels.put("ONE", 1);
-        levels.put("TWO", 2);
-        levels.put("THREE", 3);
-        levels.put("FOUR", 4);
-        levels.put("FIVE", 5);
-        levels.put("SIX", 6);
-        levels.put("SEVEN", 7);
-        levels.put("EIGHT", 8);
-        levels.put("NINE", 9);
-        levels.put("TEN", 10);
-        HYPE_CHAT_LEVELS = Collections.unmodifiableMap(levels);
-
-        HYPE_CHAT_DURATION = new Duration[] {
-            Duration.ofSeconds(30L),
-            Duration.ofSeconds(150L),
-            Duration.ofMinutes(5L),
-            Duration.ofMinutes(10L),
-            Duration.ofMinutes(30L),
-            Duration.ofHours(1L),
-            Duration.ofHours(2L),
-            Duration.ofHours(3L),
-            Duration.ofHours(4L),
-            Duration.ofHours(5L)
-        };
-    }
 }
