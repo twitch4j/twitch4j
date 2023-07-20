ddde8cb8d1d72529116dcabe1007114e75a15b60
Philipp Heuer <git@philippheuer.me>
2023-07-21 00:04:23 +0200

    chore: project version build constant (#818)
    * feat: generate project version build constant via gradle plugin
    
    * fix: remove sourceSet for buildConfig

---
+++ common/build/generated/sources/buildConfig/main/com/github/twitch4j/common/config/Twitch4JBuildConstants.java
@@ -0,0 +1,10 @@
+package com.github.twitch4j.common.config;
+
+import java.lang.String;
+
+final class Twitch4JBuildConstants {
+  public static final String VERSION = "0.0.0";
+
+  private Twitch4JBuildConstants() {
+  }
+}
