14f7843c1dce6660b415b7dc9dd2ed0089ac3ce4
iProdigy <8106344+iProdigy@users.noreply.github.com>
2023-07-22 18:28:46 -0500

    perf: split irc messages more efficiently (#821)
    Co-authored-by: Philipp Heuer <git@philippheuer.me>

--- chat/src/main/java/com/github/twitch4j/chat/util/MessageParser.java
+++ chat/src/main/java/com/github/twitch4j/chat/util/MessageParser.java
@@ -14,6 +14,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 
 @UtilityClass
 public class MessageParser {
@@ -129,4 +130,20 @@ public class MessageParser {
         tags.put(key.toString(), value);
     }
 
+    @ApiStatus.Internal
+    public void consumeLines(@NotNull String source, @NotNull Consumer<String> consumer) {
+        int len = source.length();
+        int start = 0;
+        int i;
+
+        while ((i = source.indexOf('\n', start)) >= 0) {
+            boolean carriage = i > 0 && source.charAt(i - 1) == '\r';
+            consumer.accept(source.substring(start, carriage ? i - 1 : i));
+            start = i + 1;
+        }
+
+        if (start < len) {
+            consumer.accept(source.substring(start));
+        }
+    }
 }
