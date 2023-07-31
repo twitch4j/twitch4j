8c78266dfb77f6fce2451b4d11bf49bcbc5bb8b7
iProdigy <8106344+iProdigy@users.noreply.github.com>
2023-07-31 12:20:02 -0500

    fix: avoid eventsocket relocation for module shadow jar (#827)

--- eventsub-websocket/build.gradle.kts
+++ eventsub-websocket/build.gradle.kts
@@ -9,6 +9,11 @@ dependencies {
 	api(project(":twitch4j-client-websocket"))
 }
 
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+	// Avoid com.github.twitch4j.eventsub.socket relocation due to eventsub-common being relocated
+	relocate("com.github.twitch4j.eventsub.socket", "com.github.twitch4j.eventsub.socket")
+}
+
 tasks.javadoc {
 	options {
 		title = "Twitch4J (v${version}) - EventSub WebSocket Module"
