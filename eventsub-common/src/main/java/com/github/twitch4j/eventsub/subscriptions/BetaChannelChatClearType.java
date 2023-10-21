af1aad4b116fa5328aaa3c97d75ff07b74bae510
iProdigy <gitprodigy@proton.me>
2023-10-20 23:59:42 -0700

    docs: add javadocs for new eventsub topics

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/subscriptions/BetaChannelChatClearType.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/subscriptions/BetaChannelChatClearType.java
@@ -2,7 +2,20 @@ package com.github.twitch4j.eventsub.subscriptions;
 
 import com.github.twitch4j.eventsub.condition.ChannelChatCondition;
 import com.github.twitch4j.eventsub.events.ChannelChatClearEvent;
+import org.jetbrains.annotations.ApiStatus;
 
+/**
+ * Sends a notification when a moderator or bot clears all messages from the chat room.
+ * <p>
+ * Requires user:read:chat scope from chatting user.
+ * If app access token used, then additionally requires user:bot scope from chatting user,
+ * and either channel:bot scope from broadcaster or moderator status.
+ *
+ * @see com.github.twitch4j.auth.domain.TwitchScopes#CHAT_USER_READ
+ * @see com.github.twitch4j.auth.domain.TwitchScopes#CHAT_USER_BOT
+ * @see com.github.twitch4j.auth.domain.TwitchScopes#CHAT_CHANNEL_BOT
+ */
+@ApiStatus.Experimental // in open beta
 public class BetaChannelChatClearType implements SubscriptionType<ChannelChatCondition, ChannelChatCondition.ChannelChatConditionBuilder<?, ?>, ChannelChatClearEvent> {
     @Override
     public String getName() {
--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/subscriptions/BetaChannelMessageDeleteType.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/subscriptions/BetaChannelMessageDeleteType.java
@@ -4,7 +4,18 @@ import com.github.twitch4j.eventsub.condition.ChannelChatCondition;
 import com.github.twitch4j.eventsub.events.ChannelChatMessageDeleteEvent;
 import org.jetbrains.annotations.ApiStatus;
 
-@ApiStatus.Experimental
+/**
+ * Sends a notification when a moderator removes a specific message.
+ * <p>
+ * Requires user:read:chat scope from chatting user.
+ * If app access token used, then additionally requires user:bot scope from chatting user,
+ * and either channel:bot scope from broadcaster or moderator status.
+ *
+ * @see com.github.twitch4j.auth.domain.TwitchScopes#CHAT_USER_READ
+ * @see com.github.twitch4j.auth.domain.TwitchScopes#CHAT_USER_BOT
+ * @see com.github.twitch4j.auth.domain.TwitchScopes#CHAT_CHANNEL_BOT
+ */
+@ApiStatus.Experimental // in open beta
 public class BetaChannelMessageDeleteType implements SubscriptionType<ChannelChatCondition, ChannelChatCondition.ChannelChatConditionBuilder<?, ?>, ChannelChatMessageDeleteEvent> {
     @Override
     public String getName() {
