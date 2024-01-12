3ae4abaec9eaf10f64c0e2d4a729e6c0d2b3d70a
iProdigy <8106344+iProdigy@users.noreply.github.com>
2024-01-12 14:10:02 -0600

    chore: promote ads scheduler api out of beta (#906)

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/subscriptions/ChannelAdBreakBeginType.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/subscriptions/ChannelAdBreakBeginType.java
@@ -2,7 +2,6 @@ package com.github.twitch4j.eventsub.subscriptions;
 
 import com.github.twitch4j.eventsub.condition.ChannelAdBreakCondition;
 import com.github.twitch4j.eventsub.events.ChannelAdBreakBeginEvent;
-import org.jetbrains.annotations.ApiStatus;
 
 /**
  * The channel.ad_break.begin subscription type sends a notification when a user runs a midroll commercial break,
@@ -12,8 +11,7 @@ import org.jetbrains.annotations.ApiStatus;
  *
  * @see com.github.twitch4j.auth.domain.TwitchScopes#HELIX_CHANNEL_ADS_READ
  */
-@ApiStatus.Experimental
-public class BetaChannelAdBreakBeginType implements SubscriptionType<ChannelAdBreakCondition, ChannelAdBreakCondition.ChannelAdBreakConditionBuilder<?, ?>, ChannelAdBreakBeginEvent> {
+public class ChannelAdBreakBeginType implements SubscriptionType<ChannelAdBreakCondition, ChannelAdBreakCondition.ChannelAdBreakConditionBuilder<?, ?>, ChannelAdBreakBeginEvent> {
     @Override
     public String getName() {
         return "channel.ad_break.begin";
@@ -21,7 +19,7 @@ public class BetaChannelAdBreakBeginType implements SubscriptionType<ChannelAdBr
 
     @Override
     public String getVersion() {
-        return "beta";
+        return "1";
     }
 
     @Override
