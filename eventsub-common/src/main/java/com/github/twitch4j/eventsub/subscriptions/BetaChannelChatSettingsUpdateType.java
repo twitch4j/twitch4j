8d141df246d1f27828b57e30d9da37b99782eb7d
iProdigy <8106344+iProdigy@users.noreply.github.com>
2024-01-14 13:04:02 -0600

    feat: add beta chat settings update eventsub type (#912)

---
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/subscriptions/BetaChannelChatSettingsUpdateType.java
@@ -0,0 +1,37 @@
+package com.github.twitch4j.eventsub.subscriptions;
+
+import com.github.twitch4j.eventsub.condition.ChannelChatCondition;
+import com.github.twitch4j.eventsub.events.ChannelChatSettingsUpdateEvent;
+
+/**
+ * This event sends a notification when a broadcaster’s chat settings are updated.
+ * <p>
+ * Requires user:read:chat scope from chatting user.
+ * If app access token used, then additionally requires user:bot scope from chatting user,
+ * and either channel:bot scope from broadcaster or moderator status.
+ *
+ * @see com.github.twitch4j.auth.domain.TwitchScopes#CHAT_USER_READ
+ * @see com.github.twitch4j.auth.domain.TwitchScopes#CHAT_USER_BOT
+ * @see com.github.twitch4j.auth.domain.TwitchScopes#CHAT_CHANNEL_BOT
+ */
+public class BetaChannelChatSettingsUpdateType implements SubscriptionType<ChannelChatCondition, ChannelChatCondition.ChannelChatConditionBuilder<?, ?>, ChannelChatSettingsUpdateEvent> {
+    @Override
+    public String getName() {
+        return "channel.chat_settings.update";
+    }
+
+    @Override
+    public String getVersion() {
+        return "beta";
+    }
+
+    @Override
+    public ChannelChatCondition.ChannelChatConditionBuilder<?, ?> getConditionBuilder() {
+        return ChannelChatCondition.builder();
+    }
+
+    @Override
+    public Class<ChannelChatSettingsUpdateEvent> getEventClass() {
+        return ChannelChatSettingsUpdateEvent.class;
+    }
+}
