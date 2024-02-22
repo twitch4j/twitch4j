bee63e1b2f20cf7bcd014b8c168db6d73210d74f
iProdigy <gitprodigy@proton.me>
2024-02-22 02:27:42 -0800

    fix: support refreshed tokens in pubsub module

--- pubsub/src/main/java/com/github/twitch4j/pubsub/domain/PubSubRequest.java
+++ pubsub/src/main/java/com/github/twitch4j/pubsub/domain/PubSubRequest.java
@@ -1,8 +1,12 @@
 package com.github.twitch4j.pubsub.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
 import com.github.twitch4j.pubsub.enums.PubSubType;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.jetbrains.annotations.ApiStatus;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,7 +17,7 @@ import java.util.Map;
  * Will ignore null values when serializing the request
  */
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class PubSubRequest {
 
     /**
@@ -24,11 +28,26 @@ public class PubSubRequest {
     /**
      * Random string to identify the response associated with this request.
      */
+    @EqualsAndHashCode.Exclude // topic matters more than nonce
     private String nonce;
 
     /**
      * Data (Body)
      */
-    private Map<String, Object> data = new HashMap<>();
+    @EqualsAndHashCode.Exclude // covered by getTopics()
+    private Map<String, Object> data = new HashMap<>(4);
+
+    /**
+     * Credential for {@link PubSubType#LISTEN} requests.
+     */
+    @JsonIgnore
+    @ApiStatus.Internal
+    @EqualsAndHashCode.Exclude // user_id in topic matters more than the specific credential
+    private OAuth2Credential credential;
+
+    @EqualsAndHashCode.Include // avoid duplicate LISTENs for same topic
+    private Object getTopics() {
+        return data.get("topics");
+    }
 
 }
