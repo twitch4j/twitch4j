f9622b6b8c8bfe65add86614d5413e9742cfd180
iProdigy <gitprodigy@proton.me>
2024-07-05 12:53:30 -0700

    chore: add unit tests

--- pubsub/src/main/java/com/github/twitch4j/pubsub/domain/OutboundFollow.java
+++ pubsub/src/main/java/com/github/twitch4j/pubsub/domain/OutboundFollow.java
@@ -2,13 +2,17 @@ package com.github.twitch4j.pubsub.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
 
 @Data
 @Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
 public class OutboundFollow {
 
     private String targetUserId;
--- pubsub/src/main/java/com/github/twitch4j/pubsub/domain/OutboundUnfollow.java
+++ pubsub/src/main/java/com/github/twitch4j/pubsub/domain/OutboundUnfollow.java
@@ -1,13 +1,17 @@
 package com.github.twitch4j.pubsub.domain;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
 
 @Data
 @Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
 public class OutboundUnfollow {
 
     private String targetUserId;
