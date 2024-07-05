4b3fdc45cfa02d1c7f4be0414259dfac48f5b83b
iProdigy <8106344+iProdigy@users.noreply.github.com>
2024-07-05 16:44:04 -0500

    refactor(helix): utilize ValueWrapper in more classes (#1005)

--- rest-helix/src/main/java/com/github/twitch4j/helix/domain/CharityCampaignWrapper.java
+++ rest-helix/src/main/java/com/github/twitch4j/helix/domain/CharityCampaignWrapper.java
@@ -1,32 +1,19 @@
 package com.github.twitch4j.helix.domain;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
-import java.util.List;
 import java.util.Optional;
 
-@Data
-@Setter(AccessLevel.PRIVATE)
-@NoArgsConstructor
-public class CharityCampaignWrapper {
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class CharityCampaignWrapper extends ValueWrapper<CharityCampaign> {
 
     /**
-     * A list that contains the charity campaign that the broadcaster is currently running.
-     * <p>
-     * The array is empty if the broadcaster is not running a charity campaign;
-     * the campaign information is no longer available as soon as the campaign ends.
-     */
-    private List<CharityCampaign> data;
-
-    /**
-     * @return the currently running charity campaign, in an optional wrapper.
+     * @return the currently running charity campaign, or empty if the broadcaster is not running a charity campaign.
      */
     public Optional<CharityCampaign> getCurrentCampaign() {
-        if (data == null || data.isEmpty()) return Optional.empty();
-        return Optional.ofNullable(data.get(0));
+        return Optional.ofNullable(this.get());
     }
 
 }
