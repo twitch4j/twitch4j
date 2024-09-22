8a7f55aeea8240fdbd09b7de9ac7b52117dad305
iProdigy <gitprodigy@proton.me>
2024-09-22 10:26:36 -0700

    chore: remove Mirrorable interface for DeleteMessageEvent

--- chat/src/main/java/com/github/twitch4j/chat/events/channel/DeleteMessageEvent.java
+++ chat/src/main/java/com/github/twitch4j/chat/events/channel/DeleteMessageEvent.java
@@ -14,7 +14,7 @@ import org.jetbrains.annotations.ApiStatus;
 @Value
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class DeleteMessageEvent extends AbstractChannelEvent implements MirrorableEvent {
+public class DeleteMessageEvent extends AbstractChannelEvent {
 
     /**
      * The raw message event.
