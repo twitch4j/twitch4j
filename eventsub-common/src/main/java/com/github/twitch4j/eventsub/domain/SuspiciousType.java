b9fdabe115693900899aef9eae18578430b1f3a7
iProdigy <gitprodigy@proton.me>
2025-03-20 00:01:08 -0500

    chore: clean up SuspiciousType after docs update

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/domain/SuspiciousType.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/domain/SuspiciousType.java
@@ -1,19 +1,36 @@
 package com.github.twitch4j.eventsub.domain;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Flags that caused a user to be marked as suspicious.
+ */
 public enum SuspiciousType {
 
-    @JsonAlias("manual") // what docs falsely claim
+    /**
+     * A moderator manually marked the user as restricted or monitored.
+     */
     MANUALLY_ADDED,
 
-    @JsonAlias({"ban_evader_detector", "ban_evader"}) // what docs claim (yes, both)
+    /**
+     * Twitch suspects the user is ban evading.
+     *
+     * @see <a href="https://help.twitch.tv/s/article/suspicious-user-controls?language=en_US#what">Official Help Article</a>
+     */
+    @JsonProperty("ban_evader")
     DETECTED_BAN_EVADER,
 
-    @JsonAlias("shared_channel_ban") // what docs claim
+    /**
+     * User was banned in another channel that has a ban sharing relationship with the current channel.
+     *
+     * @see <a href="https://help.twitch.tv/s/article/suspicious-user-controls?language=en_US#shared">Official Help Article</a>
+     */
     BANNED_IN_SHARED_CHANNEL,
 
+    /**
+     * User was marked as suspicious for an unknown reason; please report to our issue tracker.
+     */
     @JsonEnumDefaultValue
     UNKNOWN
 
