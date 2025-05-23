8002fd459b39e6246a4e5829c988a06db72af1f6
iProdigy <8106344+iProdigy@users.noreply.github.com>
2025-05-23 18:38:03 -0500

    fix(pubsub): add more goal types (#1146)

--- pubsub/src/main/java/com/github/twitch4j/pubsub/domain/CreatorGoal.java
+++ pubsub/src/main/java/com/github/twitch4j/pubsub/domain/CreatorGoal.java
@@ -1,10 +1,15 @@
 package com.github.twitch4j.pubsub.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Instant;
 
 @Data
 @Setter(AccessLevel.PRIVATE)
@@ -16,11 +21,50 @@ public class CreatorGoal {
     private String description;
     private Integer currentContributions;
     private Integer targetContributions;
+    private Instant createdAt;
+    private String progressBarAccentColor;
+    private String progressBarBackgroundColor;
+
+    @Nullable
+    @JsonProperty("shouldAutoIncrement")
+    @Accessors(fluent = true)
+    private Boolean shouldAutoIncrement;
 
     public enum Type {
+        /**
+         * Followers
+         */
         FOLLOWERS,
+
+        /**
+         * Bits cheers
+         */
+        NEW_BITS,
+
+        /**
+         * New cheerers
+         */
+        NEW_CHEERERS,
+
+        /**
+         * Total subscriber count
+         */
+        SUBS,
+
+        /**
+         * Total subscriber points
+         */
         SUB_POINTS,
-        NEW_SUB_POINTS
+
+        /**
+         * New subscriber points
+         */
+        NEW_SUB_POINTS,
+
+        /**
+         * New subscribers
+         */
+        NEW_SUBS
     }
 
     public enum State {
