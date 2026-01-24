1d96871a0ccd6316984bbba85aa151ef0776835d
iProdigy <gitprodigy@proton.me>
2026-01-23 18:48:49 -0600

    fix: specify SuspiciousTreatment#toString for feign serialization

--- rest-helix/src/main/java/com/github/twitch4j/helix/domain/SuspiciousTreatment.java
+++ rest-helix/src/main/java/com/github/twitch4j/helix/domain/SuspiciousTreatment.java
@@ -1,20 +1,27 @@
 package com.github.twitch4j.helix.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public enum SuspiciousTreatment {
 
     /**
      * Messages sent by monitored accounts will appear in chat,
      * but flagged in the chat panel and Mod View widget for moderators and streamers to review.
      */
-    @JsonProperty("ACTIVE_MONITORING")
-    MONITOR,
+    MONITOR("ACTIVE_MONITORING"),
 
     /**
      * Messages sent by restricted accounts will be displayed only to the streamer and their moderators.
      */
-    @JsonProperty("RESTRICTED")
-    RESTRICT
+    RESTRICT("ACTIVE_MONITORING");
 
+    private final String twitchString;
+
+    @Override
+    @JsonValue
+    public String toString() {
+        return this.twitchString;
+    }
 }
