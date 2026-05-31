7bafcc443887d081768213395c3f43a08417fb1f
Philipp Heuer <git@philippheuer.de>
2026-05-31 21:21:03 +0200

    chore: add comment to trigger ci

--- chat/src/test/java/com/github/twitch4j/chat/ChatJoinRetryTest.java
+++ chat/src/test/java/com/github/twitch4j/chat/ChatJoinRetryTest.java
@@ -18,6 +18,9 @@ import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
+/**
+ * Test class for TwitchChat's channel join retry mechanism, connects to wss://irc-ws.chat.twitch.tv:443.
+ */
 @Tag("integration")
 public class ChatJoinRetryTest {
 
