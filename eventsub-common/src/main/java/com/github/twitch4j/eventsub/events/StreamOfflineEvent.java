b1758667ba67ed5d7ef3eb66eac4f8fb1299abeb
iProdigy <gitprodigy@proton.me>
2026-06-27 21:18:14 -0500

    feat(eventsub): add id to StreamOfflineEvent

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/events/StreamOfflineEvent.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/events/StreamOfflineEvent.java
@@ -1,8 +1,22 @@
 package com.github.twitch4j.eventsub.events;
 
+import lombok.AccessLevel;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
+@Data
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class StreamOfflineEvent extends EventSubChannelEvent {}
+public class StreamOfflineEvent extends EventSubChannelEvent {
+
+    /**
+     * The id of the stream.
+     */
+    private String id;
+
+}
