e52a48e279b49525afcd078b5efe8688d00fe4c3
iProdigy <8106344+iProdigy@users.noreply.github.com>
2026-09-16 22:11:43 -0500

    feat(eventsub): add BLOCKED_LINK to AutomodCaughtReason enum

--- eventsub-common/src/main/java/com/github/twitch4j/eventsub/domain/AutomodCaughtReason.java
+++ eventsub-common/src/main/java/com/github/twitch4j/eventsub/domain/AutomodCaughtReason.java
@@ -1,5 +1,7 @@
 package com.github.twitch4j.eventsub.domain;
 
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+
 public enum AutomodCaughtReason {
 
     /**
@@ -10,6 +12,17 @@ public enum AutomodCaughtReason {
     /**
      * The message was caught for containing a blocked term.
      */
-    BLOCKED_TERM
+    BLOCKED_TERM,
+
+    /**
+     * The message contained a link and the channel has 'Hold Hyperlinks for Review' enabled.
+     */
+    BLOCKED_LINK,
+
+    /**
+     * The message was caught for an unknown reason; please report to our issue tracker.
+     */
+    @JsonEnumDefaultValue
+    UNKNOWN
 
 }
